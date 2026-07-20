27a394f35649a7e3c68b5a963550ed63e1ca8ccf
Austin Dsouza <austu16@gmail.com>
2026-07-20 16:49:02 +0530

    Initial Django Weather Chatbot setup

---
+++ weather/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+class Chat(models.Model):
+    user = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='chats'
+    )
+
+    user_message = models.TextField()
+    bot_response = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.user.username} - {self.user_message[:30]}"
+    

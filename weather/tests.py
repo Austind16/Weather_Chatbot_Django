bb7fbfddcfdb398679b8a906e2537fe73bf61cf1
Austin Dsouza <austu16@gmail.com>
2026-09-03 20:59:49 +0530

    created running tests

--- weather/tests.py
+++ weather/tests.py
@@ -1,3 +1,27 @@
 from django.test import TestCase
 
 # Create your tests here.
+import os
+import django
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
+django.setup()
+
+from weather.services.weather_service import WeatherService
+
+
+result = WeatherService.get_forecast("Mumbai")
+
+print("\n--- FORECAST TEST ---")
+print("Success:", result["success"])
+
+if result["success"]:
+    print("City:", result["city"])
+    print("Country:", result["country"])
+    print("Forecast entries:", len(result["forecast"]))
+
+    print("\nFirst forecast entry:")
+    print(result["forecast"][0])
+
+else:
+    print("Error:", result["message"])
